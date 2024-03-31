e37497fdf3338bed4afbbe4d9d3c0805d54b8fb4
Yephihy <offianoura@gmail.com>
2024-03-30 21:50:32 -0400

    button to second page

--- Perch/app.py
+++ Perch/app.py
@@ -9,5 +9,10 @@ def index():
     app_data = {'project_name': 'Perch'}
     return render_template("index.html", app_data=app_data)
 
+@app.route("/layout")
+def layout(): 
+    app_data = {'project_name': 'Perch'}  # Define app_data variable
+    return render_template("layout.html", app_data=app_data)
+    
 if __name__ == "__main__":
     app.run(debug=True)
